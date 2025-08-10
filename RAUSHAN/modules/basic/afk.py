b3cd15419622ceffcdd589de86377d49d4588376
YUKIKO426 <nfker70@gmail.com>
2025-08-10 21:18:27 +0530

    Update afk.py

--- RAUSHAN/modules/basic/afk.py
+++ RAUSHAN/modules/basic/afk.py
@@ -32,7 +32,7 @@ async def collect_afk_messages(bot: Client, message: Message):
         if GetChatID(message) not in CHAT_TYPE:
             text = (
                 f"`Beep boop. This is an automated message.\n"
-                f"I am not available right now.\n"
+                f"baad m ana bsdk.\n"
                 f"Last seen: {last_seen}\n"
                 f"Reason: ```{AFK_REASON.upper()}```\n"
                 f"See you after I'm done doing whatever I'm doing.`"
